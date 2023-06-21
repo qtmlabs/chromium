57c57fd04f92a195b207f38d117e678bff3ae86b
msizanoen <msizanoen@qtmlabs.xyz>
2023-06-21 10:43:37 +0200

    ui: Neuter system theming support
    This removes all preferences related to system theming and its
    associated setting buttons, and remove most of ThemeServiceAuraLinux.
    
    Upstreamability: Impossible

--- chrome/browser/themes/theme_service_aura_linux.h
+++ chrome/browser/themes/theme_service_aura_linux.h
@@ -22,11 +22,6 @@ class ThemeServiceAuraLinux : public ThemeService {
 
   // Overridden from ThemeService:
   ui::SystemTheme GetDefaultSystemTheme() const override;
-  void UseTheme(ui::SystemTheme system_theme) override;
-  void UseSystemTheme() override;
-  bool IsSystemThemeDistinctFromDefaultTheme() const override;
-  bool UsingSystemTheme() const override;
-  void FixInconsistentPreferencesIfNeeded() override;
 
   static ui::SystemTheme GetSystemThemeForProfile(const Profile* profile);
 };
