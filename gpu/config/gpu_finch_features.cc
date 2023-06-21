cbde0844f1f90bd81eb9e77f94822fd5e8ed77be
msizanoen <msizanoen@qtmlabs.xyz>
2023-06-21 10:43:25 +0200

    webgpu: Enable WebGPUService on Linux

--- gpu/config/gpu_finch_features.cc
+++ gpu/config/gpu_finch_features.cc
@@ -246,7 +246,7 @@ BASE_FEATURE(kEnableDrDc,
 // Enable WebGPU on gpu service side only. This is used with origin trial and
 // enabled by default on supported platforms.
 #if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_CHROMEOS) || \
-    BUILDFLAG(IS_ANDROID)
+    BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX)
 #define WEBGPU_ENABLED base::FEATURE_ENABLED_BY_DEFAULT
 #else
 #define WEBGPU_ENABLED base::FEATURE_DISABLED_BY_DEFAULT
