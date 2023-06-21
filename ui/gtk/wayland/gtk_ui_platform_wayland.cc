3a21d8c53aaf94457d1557a6662da7a43bf366e9
msizanoen <msizanoen@qtmlabs.xyz>
2023-06-21 10:43:41 +0200

    gtk: Don't set GDK_BACKEND environment variable
    GTK will take into account both the application's allowed backends list
    and the `GDK_BACKEND` environment variable when determining the backend
    to use. If the backend configured in GDK_BACKEND is not present in the
    application allowed backends list, GTK will refuse to initialize it.

--- ui/gtk/wayland/gtk_ui_platform_wayland.cc
+++ ui/gtk/wayland/gtk_ui_platform_wayland.cc
@@ -25,9 +25,6 @@ namespace gtk {
 
 GtkUiPlatformWayland::GtkUiPlatformWayland() {
   gdk_set_allowed_backends("wayland");
-  // GDK_BACKEND takes precedence over gdk_set_allowed_backends(), so override
-  // it to ensure we get the wayland backend.
-  base::Environment::Create()->SetVar("GDK_BACKEND", "wayland");
 }
 
 GtkUiPlatformWayland::~GtkUiPlatformWayland() = default;
