d5750372603bea405bcf7eaf026a23009009445e
msizanoen <msizanoen@qtmlabs.xyz>
2023-07-31 17:16:28 +0200

    ozone/wayland: Try to use an EGL device with the same DRM render node as GBM
    This ensures that both the GBM device and the EGL display will use the
    same underlying GPU to avoid accidental cross-GPU resource sharing.
    
    This currently only works with platform (non-ANGLE) EGL implementations,
    and therefore requires the validating command decoder.
    
    Upstreamability: ?????

--- ui/ozone/platform/wayland/gpu/wayland_buffer_manager_gpu.h
+++ ui/ozone/platform/wayland/gpu/wayland_buffer_manager_gpu.h
@@ -185,6 +185,10 @@ class WaylandBufferManagerGpu : public ozone::mojom::WaylandBufferManagerGpu {
     drm_modifiers_filter_ = std::move(drm_modifiers_filter);
   }
 
+  const base::FilePath& drm_render_node_path() const {
+    return drm_render_node_path_;
+  }
+
   // Adds a WaylandBufferManagerGpu binding.
   void AddBindingWaylandBufferManagerGpu(
       mojo::PendingReceiver<ozone::mojom::WaylandBufferManagerGpu> receiver);
@@ -270,6 +274,8 @@ class WaylandBufferManagerGpu : public ozone::mojom::WaylandBufferManagerGpu {
   void OpenAndStoreDrmRenderNodeFd(const base::FilePath& drm_node_path);
   // Used by the gbm_device for self creation.
   base::ScopedFD drm_render_node_fd_;
+  // Used for EGL device selection.
+  base::FilePath drm_render_node_path_;
   // A DRM render node based gbm device.
   std::unique_ptr<GbmDevice> gbm_device_;
   // When set, avoids creating a real gbm_device. Instead, tests that set
