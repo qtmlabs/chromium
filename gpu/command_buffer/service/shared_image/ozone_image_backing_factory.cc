35d516ce6c5cda7144523efb480d99ba10d849a6
msizanoen <msizanoen@qtmlabs.xyz>
2023-08-08 06:15:52 +0200

    ozone: Workaround for importing YCbCr native pixmaps on Mesa GBM
    Mesa currently can't tell us if a given YUV format can be imported, but
    we know that some of them are supported if certain others are.
    
    This is meant as a temporary workaround until
    https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23214 lands
    upstream.

--- gpu/command_buffer/service/shared_image/ozone_image_backing_factory.cc
+++ gpu/command_buffer/service/shared_image/ozone_image_backing_factory.cc
@@ -267,7 +267,8 @@ bool OzoneImageBackingFactory::IsSupported(
   }
   auto* factory = ui::OzonePlatform::GetInstance()->GetSurfaceFactoryOzone();
   if (viz::HasEquivalentBufferFormat(format) &&
-      !factory->CanCreateNativePixmapForFormat(ToBufferFormat(format))) {
+      !factory->CanCreateNativePixmapForFormat(
+          ToBufferFormat(format), gmb_type == gfx::NATIVE_PIXMAP)) {
     return false;
   }
 
