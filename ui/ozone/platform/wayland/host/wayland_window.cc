68943d4a6832c92cdc4c3c975cdf11a110a3dc19
msizanoen <msizanoen@qtmlabs.xyz>
2023-08-08 06:15:27 +0200

    ozone/wayland: Ensure that wl_pointer::enter event updates the cursor
    The WaylandWindowManager's current pointer focused window is updated
    directly by WaylandDataDragController and WaylandWindowDragController.
    This causes the cursor to not be updated on drop as
    OnPointerFocusChanged might not be called on wl_pointer::enter event.
    
    Fix this by only updating the cursor when a wl_pointer::event actually
    happens instead of when notified by WaylandWindowManager.

--- ui/ozone/platform/wayland/host/wayland_window.cc
+++ ui/ozone/platform/wayland/host/wayland_window.cc
@@ -314,17 +314,16 @@ std::optional<float> WaylandWindow::GetPreferredScaleFactor() const {
   return root_surface_->preferred_scale_factor();
 }
 
-void WaylandWindow::OnPointerFocusChanged(bool focused) {
-  // Whenever the window gets the pointer focus back, the cursor shape must be
-  // updated. Otherwise, it is invalidated upon wl_pointer::leave and is not
-  // restored by the Wayland compositor.
+void WaylandWindow::OnPointerFocusChanged(bool focused) {}
+
+void WaylandWindow::ForceUpdateCursor() {
 #if BUILDFLAG(IS_LINUX)
-  if (focused && async_cursor_) {
+  if (async_cursor_) {
     async_cursor_->AddCursorLoadedCallback(base::BindOnce(
         &WaylandWindow::OnCursorLoaded, AsWeakPtr(), async_cursor_));
   }
 #else
-  if (focused && cursor_) {
+  if (cursor_) {
     UpdateCursorShape(cursor_);
   }
 #endif
