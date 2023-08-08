f338f3c0a6edbacbbefe9fe4efed4805ef41effa
msizanoen <msizanoen@qtmlabs.xyz>
2023-08-08 06:16:37 +0200

    gl: Pass the native platform type to ANGLE in GetDisplayFromType
    This eliminates the need to manually set
    EGL_PLATFORM_ANGLE_NATIVE_PLATFORM_TYPE_ANGLE in X11GLEGLUtility or
    through the EGL_PLATFORM environment variable for Ozone platforms,
    instead specifying the desired native platform type in the platform's
    GLOzoneEGL::GetNativeDisplay() method.
    
    This CL also deletes wayland_gl_egl_utility.cc as it's no longer needed.

--- ui/ozone/platform/wayland/host/wayland_connection.cc
+++ ui/ozone/platform/wayland/host/wayland_connection.cc
@@ -28,6 +28,7 @@
 #include "ui/events/devices/touchscreen_device.h"
 #include "ui/events/ozone/layout/keyboard_layout_engine_manager.h"
 #include "ui/gfx/geometry/point.h"
+#include "ui/gl/gl_bindings.h"
 #include "ui/ozone/common/features.h"
 #include "ui/ozone/platform/wayland/common/wayland_object.h"
 #include "ui/ozone/platform/wayland/common/wayland_util.h"
@@ -804,6 +805,11 @@ struct wl_callback* WaylandConnection::GetSyncCallback() {
 }
 
 gl::EGLDisplayPlatform WaylandConnection::GetNativeDisplay() {
+  if (gl::g_driver_egl.client_ext.b_EGL_EXT_platform_wayland) {
+    return gl::EGLDisplayPlatform(
+        reinterpret_cast<EGLNativeDisplayType>(display()),
+        EGL_PLATFORM_WAYLAND_EXT);
+  }
   return gl::EGLDisplayPlatform(
       reinterpret_cast<EGLNativeDisplayType>(display()));
 }
