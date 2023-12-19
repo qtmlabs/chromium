238db648cf5275f409a84f3398cbf718320ef935
msizanoen <msizanoen@qtmlabs.xyz>
2023-12-19 14:17:25 +0100

    gpu: Allow toggling GPU sandbox from about:flags
    Upstreamability: lol

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -11818,6 +11818,12 @@ const FeatureEntry kFeatureEntries[] = {
      kOsLinux | kOsCrOS,
      SINGLE_VALUE_TYPE(switches::kVulkanForceYCbCrLinearSampling)},
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+
+#if BUILDFLAG(IS_LINUX)
+    {"disable-gpu-sandbox", flag_descriptions::kDisableGpuSandboxName,
+     flag_descriptions::kDisableGpuSandboxDescription, kOsLinux,
+     SINGLE_VALUE_TYPE(sandbox::policy::switches::kDisableGpuSandbox)},
+#endif
     // NOTE: Adding a new flag requires adding a corresponding entry to enum
     // "LoginCustomFlags" in tools/metrics/histograms/enums.xml. See "Flag
     // Histograms" in tools/metrics/histograms/README.md (run the
