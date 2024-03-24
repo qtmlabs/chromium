37049ec62cc948accaefd4e4426c7fa70f1705c4
msizanoen <msizanoen@qtmlabs.xyz>
2024-03-24 15:49:42 +0100

    ui: Disable EyeDropper on Linux

--- ui/base/ui_base_features.cc
+++ ui/base/ui_base_features.cc
@@ -325,8 +325,7 @@ bool IsForcedColorsEnabled() {
 // milestones.
 BASE_FEATURE(kEyeDropper,
              "EyeDropper",
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
-    BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_CHROMEOS)
              base::FEATURE_ENABLED_BY_DEFAULT
 #else
              base::FEATURE_DISABLED_BY_DEFAULT
