b15002c74edf24ef24226a57b6714626e7fbc45c
Myrrh Periwinkle <myrrhperiwinkle@qtmlabs.xyz>
2025-06-17 10:48:29 +0700

    ozone/wayland: Defer destruction of clipboard data source on replace
    Chromium's current behavior of destroying the current clipboard data
    source and immediately replacing it with a new one is causing race
    condition issues with certain clipboard managers.
    
    Fix this by deferring the destruction of the clipboard data source to
    after the new data source is set as the clipboard data source.

--- ui/ozone/platform/wayland/host/wayland_clipboard.cc
+++ ui/ozone/platform/wayland/host/wayland_clipboard.cc
@@ -144,6 +144,9 @@ class ClipboardImpl final : public Clipboard, public DataSource::Delegate {
       offered_data_.clear();
       source_.reset();
     } else {
+      // Don't destroy the current source before the new source is ready.
+      auto old_source = std::move(source_);
+
       offered_data_ = *data;
 
 #if BUILDFLAG(IS_LINUX)
