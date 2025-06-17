b5f1f240c4616f773e2f07193e4058a9ed83e511
Myrrh Periwinkle <myrrhperiwinkle@qtmlabs.xyz>
2025-06-17 10:48:37 +0700

    Enable modern desktop Linux features by default

--- ui/base/ui_base_features.cc
+++ ui/base/ui_base_features.cc
@@ -149,7 +149,7 @@ BASE_FEATURE(kWaylandSessionManagement,
 COMPONENT_EXPORT(UI_BASE_FEATURES)
 BASE_FEATURE(kOverrideDefaultOzonePlatformHintToAuto,
              "OverrideDefaultOzonePlatformHintToAuto",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_LINUX)
 
 // Chrome for Linux should eventually use XInput2 key events.
