4671dd6d94804c4ad1d7419d979521192df86133
Myrrh Periwinkle <myrrhperiwinkle@qtmlabs.xyz>
2025-06-17 10:48:11 +0700

    vulkan: Introduce `--vulkan-force-ycbcr-linear-sampling` flag

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -293,6 +293,7 @@ static const char* const kSwitchNames[] = {
     switches::kUseAdapterLuid,
     switches::kUseFakeMjpegDecodeAccelerator,
     switches::kUseGpuInTests,
+    switches::kVulkanForceYCbCrLinearSampling,
     switches::kWebViewDrawFunctorUsesVulkan,
     switches::kSuppressPerformanceLogs,
 #if BUILDFLAG(IS_MAC)
