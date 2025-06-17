b1e3f78e10550e62c6dde6e59fc0862513e2dcbc
Myrrh Periwinkle <myrrhperiwinkle@qtmlabs.xyz>
2025-06-17 10:48:11 +0700

    vulkan: Introduce `--vulkan-force-ycbcr-linear-sampling` flag

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -13235,6 +13235,14 @@ const FeatureEntry kFeatureEntries[] = {
          "LensOverlayTextSelectionContextMenuEntrypoint")},
 #endif  // !BUILDFLAG(IS_ANDROID)
 
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+    {"vulkan-force-ycbcr-linear-sampling",
+     flag_descriptions::kVulkanForceYCbCrLinearSamplingName,
+     flag_descriptions::kVulkanForceYCbCrLinearSamplingDescription,
+     kOsLinux | kOsCrOS,
+     SINGLE_VALUE_TYPE(switches::kVulkanForceYCbCrLinearSampling)},
+#endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+
     // Add new entries above this line.
 
     // NOTE: Adding a new flag requires adding a corresponding entry to enum
