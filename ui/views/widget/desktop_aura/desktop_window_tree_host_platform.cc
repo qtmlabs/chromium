d35d6d35aec0f3a7d145ac9c2268ab22543ec75a
Myrrh Periwinkle <myrrhperiwinkle@qtmlabs.xyz>
2025-07-29 18:53:01 +0700

    aura: Always render fullscreen windows as opaque on Linux

--- ui/views/widget/desktop_aura/desktop_window_tree_host_platform.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_platform.cc
@@ -776,6 +776,9 @@ bool DesktopWindowTreeHostPlatform::ShouldUseNativeFrame() const {
 }
 
 bool DesktopWindowTreeHostPlatform::ShouldWindowContentsBeTransparent() const {
+  if (IsFullscreen()) {
+    return false;
+  }
   return platform_window()->ShouldWindowContentsBeTransparent() ||
          !(GetWindowMaskForClipping().isEmpty());
 }
