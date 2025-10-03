daad12ed12322d6003f61cefdf4e3533ef8bd064
Myrrh Periwinkle <myrrhperiwinkle@qtmlabs.xyz>
2025-10-03 16:33:51 +0700

    ozone/wayland: Properly handle unset per-surface color space

--- ui/ozone/platform/wayland/host/wayland_wp_color_management_surface.cc
+++ ui/ozone/platform/wayland/host/wayland_wp_color_management_surface.cc
@@ -115,11 +115,9 @@ void WaylandWpColorManagementSurface::OnHdrEnabledChanged(bool hdr_enabled) {
     return;
   }
 
-  auto display_color_spaces = hdr_enabled ? display_color_spaces_ : nullptr;
   root->OnDisplayColorSpacesChanged(
-      display_color_spaces
-          ? display_color_spaces
-          : base::MakeRefCounted<gfx::DisplayColorSpacesRef>());
+      hdr_enabled ? display_color_spaces_
+                  : base::MakeRefCounted<gfx::DisplayColorSpacesRef>());
 }
 
 }  // namespace ui
